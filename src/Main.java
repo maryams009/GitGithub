b155998c1a1278bccb328eb55651867945903e35
MaryamSaddiqa <Maryamsaddiqa7@gmail.com>
2022-11-09 13:32:37 +0100

    First commit of the project with all the files

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello class");
+        MethodClass methodclass = new MethodClass();
+        methodclass.fullname();
+    }
+}
---
+++ src/MethodClass.java
@@ -0,0 +1,10 @@
+public class MethodClass {
+
+        String fname = "Maryam";
+        String lname = "Saddiqa";
+        void fullname(){
+            System.out.println("My name is");
+            System.out.println(fname+" " +lname);
+        }
+    }
+
